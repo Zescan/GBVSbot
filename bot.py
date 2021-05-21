7b461ee3358b6e5a7806c424c355997bec47d192
crew852 <65230973+crew852@users.noreply.github.com>
2021-05-22 05:46:45 +0900

    slash roll-back

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
-from discord_slash import SlashCommand
-
+from discord.ext import commands
 import os
+
 import asyncio
 import numpy as np
 import random
@@ -13,15 +13,13 @@ import embed as blow
 import changer as cg
 import kor_changer as kcg
 
-bot = discord.Client(intents=discord.Intents.all())
-slash = SlashCommand(bot, sync_commands=True)
-# 캐릭 목록
+bot = commands.Bot(command_prefix='>', help_command=None)
+
 charlist_path = os.path.dirname(os.path.abspath(__file__)) + "/캐릭목록.txt"
 o = open(charlist_path, "r", encoding="utf-8")
 charlist = o.read().split()
-# 서버아이디
-guild_ids = bot.guilds
-#database
+
+
 dab = sqlite3.connect("./framedata.db")
 dbb = sqlite3.connect("sklist.db")
 
@@ -30,33 +28,33 @@ async def on_ready():
     print('다음으로 로그인합니다: ')
     print(bot.user.name)
     print('connection was succesful')
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("'/명령어'로 기능확인"))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("'>명령어'로 기능확인"))
 
 
 # commands
 
 
-@slash.slash(name="명령어", description='명령어 목록을 보여줍니다.', guild_ids=guild_ids)
-async def list(ctx):
-    embed=discord.Embed(title='명령어 목록', description='-괄호 안의 인자도 같이 써주세요!', color=0xfd4949)
-    embed.add_field(name='/설명서', value='파스티바_봇 사용설명서를 보여줍니다.', inline=False)
-    embed.add_field(name='/핑', value='현재 핑 상태를 측정합니다.', inline=False)
-    embed.add_field(name='/랜덤', value='랜덤으로 아무 캐릭터나 뽑아줍니다.', inline=False)
-    embed.add_field(name='/캐릭터', value='캐릭터들 목록과 영문 이름을 보여줍니다.', inline=False)
-    embed.add_field(name='/기술 (캐릭명)', value='해당 캐릭터의 기술 목록을 보여줍니다.', inline=False)
-    embed.add_field(name='/공략 (캐릭명)', value='해당 캐릭터의 공략글을 보여줍니다.', inline=False)
-    embed.add_field(name='/검색 (캐릭명) (기술 커맨드)', value='해당 캐릭터의 기술의 프레임데이터를 보여줍니다.', inline=False)
-    embed.add_field(name='/깃허브', value='파스티바_봇의 깃허브 링크를 보여줍니다.', inline=False)
+@bot.command(aliases=['help'])
+async def 명령어(ctx):
+    embed=discord.Embed(title='명령어 목록', description='-괄호 안에 인자도 같이 써주세요!', color=0xfd4949)
+    embed.add_field(name='>tip\n>설명서', value='파스티바_봇 사용설명서를 보여줍니다.', inline=False)
+    embed.add_field(name='>핑\n>ping', value='현재 핑 상태를 측정합니다.', inline=False)
+    embed.add_field(name='>랜덤\n>r', value='랜덤으로 아무 캐릭터나 뽑아줍니다.', inline=False)
+    embed.add_field(name='>캐릭터\n>char', value='캐릭터들 목록과 영문 이름을 보여줍니다.', inline=False)
+    embed.add_field(name='>기술 (캐릭명)\n>m (캐릭명)', value='해당 캐릭터의 기술 목록을 보여줍니다.', inline=False)
+    embed.add_field(name='>공략 (캐릭명)\n>g (캐릭명)', value='해당 캐릭터의 공략글을 보여줍니다.', inline=False)
+    embed.add_field(name='>i (캐릭명) (기술 커맨드)\n>검색 (캐릭명) (기술 커맨드)', value='해당 캐릭터의 기술의 프레임데이터를 보여줍니다.', inline=False)
+    embed.add_field(name='>깃허브\n>githib', value='파스티바_봇의 깃허브 링크를 보여줍니다.', inline=False)
     await ctx.send(embed=embed)
 
-@slash.slash(name="핑", description='현재 핑 상태를 측정합니다.', guild_ids=guild_ids)
+@bot.command(aliases=['핑'])
 async def ping(ctx):
     latancy = bot.latency
     embed=discord.Embed(title='현재 ping상태는...', description=f'{round(latancy * 1000)}ms 입니다.', color=0xfd4949)
     await ctx.send(embed=embed)
 
-@slash.slash(name="설명서", description='파스티바_봇 사용설명서를 보여줍니다.', guild_ids=guild_ids)
-async def tip(ctx):
+@bot.command(aliases=['tip'])
+async def 설명서(ctx):
     embed=discord.Embed(title="파스티바_봇 사용 설명서", description="제작 - Rolling_Pumpkin", color=0x44e456)
     embed.add_field(name="#프레임데이터", value="파스티바_봇은 Dustloop wiki 에서 프레임 데이터를 가져왔습니다", inline=False)
     embed.add_field(name="#대,소문자", value="검색기능을 영어로 이용하실 때는 가능한 모두 소문자로 입력해주세요", inline=False)
@@ -69,8 +67,8 @@ async def tip(ctx):
     embed.add_field(name="#피드백", value="틀린 부분, 추가할 부분, 개선할 부분은 @Rolling_Pumpkin 으로 말씀해주세요", inline=False)
     await ctx.send(embed=embed)
 
-@slash.slash(name="캐릭터", description='캐릭터들 목록과 영문 이름을 보여줍니다.', guild_ids=guild_ids)
-async def char(ctx):
+@bot.command(aliases=['char'])
+async def 캐릭터(ctx):
     embed=discord.Embed(title="캐릭터 리스트", description="현재 검색 가능한 캐릭터 목록입니다.", color=0x6b9fff)
     embed.add_field(name="그랑", value="Gran", inline=True)
     embed.add_field(name="나루메아", value="Narmaya", inline=True)
@@ -95,10 +93,11 @@ async def char(ctx):
     embed.add_field(name="페리", value="Ferry", inline=True)
     await ctx.send(embed=embed)
 
-@slash.slash(name="검색", description='해당 캐릭터의 기술의 프레임데이터를 보여줍니다.', guild_ids=guild_ids)
+@bot.command(aliases=['검색', 'I'])
 async def i(ctx, charname, command):
     charname = ncg.ncgr(charname)
     charname = charname.capitalize()
+    # command = command.lower()
     command = cg.cgr(command)
     command = kcg.nor_cgr(command)
     k = '12'
@@ -127,12 +126,13 @@ async def i(ctx, charname, command):
         '히트시 이득': row[9]}
         await blow.t_embed(ctx, charname + " - " + command, row[2], info_dic)
 
-@slash.slash(name='기술', description='해당 캐릭터의 기술 목록을 보여줍니다.', guild_ids=guild_ids)
+@bot.command(aliases=['기술'])
 async def m(ctx, character):
     character = ncg.ncgr(character)
     charname = character.capitalize()
     print(charname)
     query_ = "WHERE charname = '" + charname + "'"
+    # print(query_)
     rows = db.db_sktable(dbb,query_)
     if not rows:
         embed=discord.Embed(title="해당하는 정보를 찾을 수 없습니다", description="다시 한 번 확인해 주세요", color=0xedf11e)
@@ -144,7 +144,7 @@ async def m(ctx, character):
             info_['커맨드'].append(row[2])
         await blow.c_embed(ctx, charname, " 기술 목록 ", info_)
 
-@slash.slash(name="공략", description='해당 캐릭터의 공략글을 보여줍니다.', guild_ids=guild_ids)
+@bot.command(aliases=['공략', 'G'])
 async def g(ctx, charname):
     charname = ncg.rncgr(charname)
     if charname in charlist:
@@ -153,13 +153,13 @@ async def g(ctx, charname):
         embed=discord.Embed(title="해당하는 정보를 찾을 수 없습니다", description="다시 한 번 확인해 주세요", color=0xedf11e)
         await ctx.send(embed=embed)
 
-@slash.slash(name="랜덤", description='랜덤으로 아무 캐릭터나 뽑아줍니다.', guild_ids=guild_ids)
+@bot.command(aliases=['랜덤'])
 async def r(ctx):
     choicechar = random.choice(charlist)
     embed = discord.Embed(title="캐릭터 랜덤 선택 결과...", description=f'[{choicechar}] (이)가 나왔습니다.', color=0xb377ee)
     await ctx.send(embed=embed)
 
-@slash.slash(name="깃허브", description='파스티바_봇의 깃허브 링크를 보여줍니다.', guild_ids=guild_ids)
+@bot.command(aliases=['깃허브'])
 async def github(ctx):
     embed = discord.Embed(title="파스티바_봇 깃허브 링크", description="https://github.com/crew852/GBVSbot", color=0xb377ee)
     await ctx.send(embed=embed)
