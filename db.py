93b7e32188fe490205b834a5a45c7c4ceaf5d9c1
kh2human <khorzkor@gmail.com>
2021-12-25 15:35:56 +0900

    디비 관리를 봇 메인이 아닌 디비 모듈에서 사용하도록 합니다.

--- db.py
+++ db.py
@@ -1,27 +1,55 @@
 import sqlite3
+import warnings
 
 # print(sqlite3.version)
 # print(sqlite3.sqlite_version)
 
 framedata = 'framedata'
 
-def db_table(db, query_str=''):
-    with db:
-        cur = db.cursor()
-        if bool(query_str):
-            query_str = ' ' + query_str
-        execute_str = 'SELECT * FROM framedata' + query_str
-        cur.execute(execute_str)
-        rows = cur.fetchall()
-        return rows
+db = sqlite3.connect("./framedata.db")
+
+def db_table(query_str=''):
+		with db:
+				cur = db.cursor()
+				if bool(query_str):
+						query_str = ' ' + query_str
+				execute_str = 'SELECT * FROM framedata' + query_str
+				cur.execute(execute_str)
+				rows = cur.fetchall()
+				return rows
+
+
+def _db_table(db, query_str=''):
+		with db:
+			warnings.warn("deprecated")
+			cur = db.cursor()
+			if bool(query_str):
+					query_str = ' ' + query_str
+			execute_str = 'SELECT * FROM framedata' + query_str
+			cur.execute(execute_str)
+			rows = cur.fetchall()
+			return rows
 
 # db>데이터베이스 연결 , query>캐릭명
-def db_sktable(db, query_=''):
-    with db:
-        cur = db.cursor()
-        if bool(query_):
-            query_ = ' ' + query_
-        execute_ = 'SELECT * FROM framedata' + query_
-        cur.execute(execute_)
-        rows = cur.fetchall()
-        return rows
+def db_sktable(query_=''):
+		with db:
+				cur = db.cursor()
+				if bool(query_):
+						query_ = ' ' + query_
+				execute_ = 'SELECT * FROM framedata' + query_
+				cur.execute(execute_)
+				rows = cur.fetchall()
+				return rows
+
+
+# db>데이터베이스 연결 , query>캐릭명
+def _db_sktable(db, query_=''):
+		with db:
+			warnings.warn("deprecated")
+			cur = db.cursor()
+			if bool(query_):
+					query_ = ' ' + query_
+			execute_ = 'SELECT * FROM framedata' + query_
+			cur.execute(execute_)
+			rows = cur.fetchall()
+			return rows
