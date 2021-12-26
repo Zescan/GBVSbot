63b37af8a720751497773840e148227d1e8300f9
kh2human <khorzkor@gmail.com>
2021-12-26 17:26:21 +0900

    디비를 이용한 이름 처리를 추가합니다.

--- db.py
+++ db.py
@@ -8,12 +8,14 @@ import warnings
 con = sqlite3.connect("./db.db")
 con.row_factory = sqlite3.Row
 
-def db_table(query_str=''):
+
+def framedata(query_str=''):
 		with con:
 				cur = con.cursor()
 				if bool(query_str):
 						query_str = ' ' + query_str
-				execute_str = 'select guard.ko as guard_ko, _framedata.* from (SELECT * FROM framedata ' + query_str + " ) _framedata left join guard on (_framedata.guard = guard.en)"
+				execute_str = 'select guard.ko as guard_ko, name.ko as name_ko, _framedata.* from (SELECT * FROM framedata ' + query_str + " ) _framedata left join guard on (_framedata.guard = guard.en) "
+				execute_str += " left join name on (_framedata.charname = name.en) order by odr "
 				cur.execute(execute_str)
 				row = cur.fetchone()
 				if row:
@@ -37,6 +39,7 @@ def _db_table(db, query_str=''):
 # db>데이터베이스 연결 , query>캐릭명
 def db_sktable(query_=''):
 		with con:
+				warnings.warn("deprecated")
 				cur = con.cursor()
 				if bool(query_):
 						query_ = ' ' + query_
@@ -66,7 +69,25 @@ def _db_sktable(db, query_=''):
 def char():
 	with con:
 		cur = con.cursor()
-		query = 'select distinct name_ko, charname from framedata order by name_ko, charname'
+		query = 'select name.ko as name_ko, _framedata.charname from (select distinct charname from framedata) _framedata left join name on (_framedata.charname = name.en) order by name_ko, charname'
 		cur.execute(query)
 		rows = cur.fetchall()
 		return rows
+
+
+def en(ko):
+	with con:
+		cur = con.cursor()
+		query = "select en from name where ko = '{ko}'".format(ko=ko)
+		cur.execute(query)
+		row = cur.fetchone()
+		return row['en'] or None
+
+
+def ko(en):
+	with con:
+		cur = con.cursor()
+		query = "select ko from name where en = '{en}'".format(en=en)
+		cur.execute(query)
+		row = cur.fetchone()
+		return row['ko'] or None
