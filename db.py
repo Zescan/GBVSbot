c5ddd8aa7965abc8b9a63ea11735d94549b464a0
kh2human <khorzkor@gmail.com>
2022-01-05 20:06:00 +0900

    공통 패턴에 대한 변환 처리를 변경합니다.

--- db.py
+++ db.py
@@ -118,7 +118,8 @@ def command(pattern):
 		cur = con.cursor()
 		cur.execute("select * from command order by length(pattern) desc, length(replace) DESC, pattern desc, replace desc")
 		for row in cur.fetchall():
-			replace = replace.replace(row['pattern'], row['replace'])
+			# replace = replace.replace(row['pattern'], row['replace'])
+			replace = re.sub(re.compile(row['pattern']), row['replace'], replace)
 		return replace
 
 
