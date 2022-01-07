fa5b86054ae58b61092acf84ca1eeefacc4835b3
kh2human <khorzkor@gmail.com>
2022-01-08 00:05:58 +0900

    * 미사용 함수를 주석 처리합니다. * 차후 삭제 예정입니다.

--- db.py
+++ db.py
@@ -3,8 +3,6 @@ import re
 import sqlite3
 import warnings
 
-# print(sqlite3.version)
-# print(sqlite3.sqlite_version)
 con = sqlite3.connect("./db.db")
 con.row_factory = sqlite3.Row
 
@@ -23,46 +21,42 @@ def framedata(query_str=''):
 				rows = cur.fetchall()
 				return rows
 
-
-def _db_table(db, query_str=''):
-		with db:
-			warnings.warn("deprecated")
-			cur = db.cursor()
-			if bool(query_str):
-					query_str = ' ' + query_str
-			execute_str = 'SELECT * FROM framedata' + query_str
-			cur.execute(execute_str)
-			rows = cur.fetchall()
-			return rows
+# def _db_table(db, query_str=''):
+# 		with db:
+# 			cur = db.cursor()
+# 			if bool(query_str):
+# 					query_str = ' ' + query_str
+# 			execute_str = 'SELECT * FROM framedata' + query_str
+# 			cur.execute(execute_str)
+# 			rows = cur.fetchall()
+# 			return rows
 
 # db>데이터베이스 연결 , query>캐릭명
-def db_sktable(query_=''):
-		with con:
-				warnings.warn("deprecated")
-				cur = con.cursor()
-				if bool(query_):
-						query_ = ' ' + query_
-				execute_ = 'SELECT * FROM framedata' + query_
-				cur.execute(execute_)
-				row = cur.fetchone()
-				if row:
-					logging.debug(row.keys())
-				cur.execute(execute_)
-				rows = cur.fetchall()
-				return rows
+# def db_sktable(query_=''):
+# 		with con:
+# 				cur = con.cursor()
+# 				if bool(query_):
+# 						query_ = ' ' + query_
+# 				execute_ = 'SELECT * FROM framedata' + query_
+# 				cur.execute(execute_)
+# 				row = cur.fetchone()
+# 				if row:
+# 					logging.debug(row.keys())
+# 				cur.execute(execute_)
+# 				rows = cur.fetchall()
+# 				return rows
 
 
 # db>데이터베이스 연결 , query>캐릭명
-def _db_sktable(db, query_=''):
-		with db:
-			warnings.warn("deprecated")
-			cur = db.cursor()
-			if bool(query_):
-					query_ = ' ' + query_
-			execute_ = 'SELECT * FROM framedata' + query_
-			cur.execute(execute_)
-			rows = cur.fetchall()
-			return rows
+# def _db_sktable(db, query_=''):
+# 		with db:
+# 			cur = db.cursor()
+# 			if bool(query_):
+# 					query_ = ' ' + query_
+# 			execute_ = 'SELECT * FROM framedata' + query_
+# 			cur.execute(execute_)
+# 			rows = cur.fetchall()
+# 			return rows
 
 
 def char():
