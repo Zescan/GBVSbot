688dc76c776627b7d8f6c46c76463480ea80ac74
DEEPDIVE <khorzkor@gmail.com>
2022-02-09 10:32:34 +0900

    불필요 import 및 주석을 제거합니다.

--- bot.py
+++ bot.py
@@ -1,11 +1,7 @@
-import asyncio
 import logging
 import os
 import random
 import re
-import sqlite3
-import sys
-import warnings
 
 import discord
 from discord.ext import commands
@@ -13,11 +9,7 @@ from discord_slash import SlashCommand
 from dotenv import load_dotenv
 
 import blow
-# import changer
 import db
-# import kor_changer
-# import nchanger
-import numpy as np
 
 load_dotenv()
 
@@ -99,10 +91,6 @@ async def on_message(message):
 			await _ping(ctx)
 	elif first in ["랜덤", "루나루", "Lunalu", "lunalu"]:
 			await _random(ctx)
-	# elif first == "루나루":
-	# 		await _random(ctx)
-	# elif first == "Lunalu":
-	# 		await _random(ctx)
 	elif first == "캐릭터":
 			await _char(ctx)
 	elif first == "공략":
@@ -124,9 +112,8 @@ async def on_message(message):
 
 @cmdbot.command()
 async def pass_normal(ctx):
-	logging.info("이것은 무엇인가?")
+	logging.error("이것은 무엇인가?")
 	pass
-# commands
 
 
 @slash.slash(name="명령어", description='명령어 목록을 보여줍니다.', guild_ids=guild_ids)
@@ -137,16 +124,6 @@ async def _list(ctx):
 
 async def __list(ctx):
 		embed = discord.Embed(title='명령어 목록', description='-괄호 안의 인자도 같이 써주세요!', color=0xfd4949)
-	# embed.add_field(name='/설명서', value='파스티바_봇 사용설명서를 보여줍니다.', inline=False)
-	# embed.add_field(name='/핑', value='현재 핑 상태를 측정합니다.', inline=False)
-	# embed.add_field(name='/랜덤', value='랜덤으로 아무 캐릭터나 뽑아줍니다.', inline=False)
-	# embed.add_field(name='/루나루', value='랜덤으로 아무 캐릭터나 뽑아줍니다.', inline=False)
-	# embed.add_field(name='/Lunalu', value='랜덤으로 아무 캐릭터나 뽑아줍니다.', inline=False)
-	# embed.add_field(name='/캐릭터', value='캐릭터들 목록과 영문 이름을 보여줍니다.', inline=False)
-	# embed.add_field(name='/공략 (캐릭명)', value='해당 캐릭터의 공략글을 보여줍니다.', inline=False)
-	# embed.add_field(name='/검색 (캐릭명) (기술 커맨드)', value='해당 캐릭터의 기술의 프레임데이터를 보여줍니다.', inline=False)
-	# embed.add_field(name='/기술 (캐릭명) (기술 커맨드)', value='해당 캐릭터의 기술의 프레임데이터를 보여줍니다.', inline=False)
-	# embed.add_field(name='/깃허브', value='파스티바_봇의 깃허브 링크를 보여줍니다.', inline=False)
 		for row in db._list():
 			embed.add_field(name='/{name}'.format(name=row['name']), value=row['value'], inline=False)
 		await ctx.send(embed=embed)
@@ -171,16 +148,13 @@ async def tip(ctx):
 
 
 async def _tip(ctx):
-		embed = discord.Embed(title="파스티바_봇 사용 설명서", description="제작 - Rolling_Pumpkin", color=0x44e456)
-		embed.add_field(name="#프레임데이터", value="파스티바_봇은 Dustloop wiki 에서 프레임 데이터를 가져왔습니다", inline=False)
-		embed.add_field(name="#대,소문자", value="검색기능을 영어로 이용하실 때는 가능한 모두 소문자로 입력해주세요", inline=False)
-		embed.add_field(name="#한·영사용", value="기술 커맨드의 방향 표시의 숫자를 제외하면 모두 한·영 구분없이 사용하셔도 괜찮습니다", inline=False)
-		embed.add_field(name="#버튼 입력", value="약공격은 l (light)\n중공격은 m (middle)\n강공격은 h (heavy)\n아무 버튼이든 상관없는 경우 x (예시 : 236x)\n모으기 커맨드는 따로 표시할 필요없이 방향만 입력해주세요 \n(예시 : 샤를로테 글리터 온슬로트 커맨드 -> 46h)", inline=False)
-		embed.add_field(name="#특수 기술", value="잡기는 tr (throw), 오버헤드는 oh (overhead) 또는 mh (커맨드) 로 입력해주세요", inline=False)
-		embed.add_field(name="#공중 사용 기술", value="공중에서 사용하는 기술은 커맨드 앞에 j 를 붙여주세요", inline=False)
-		embed.add_field(name="#근,원거리 기본기", value="근거리는 공격키 앞에 c (close), 원거리는 f (far)를 붙여주세요", inline=False)
-		embed.add_field(name="검색예시)", value=">검색 그랑 214H (그랑-드라이브 버스트 강버젼)\n >i metera fm (메테라 원거리 중)", inline=False)
-		embed.add_field(name="#피드백", value="틀린 부분, 추가할 부분, 개선할 부분은 @Rolling_Pumpkin 으로 말씀해주세요", inline=False)
+		embed = discord.Embed(title="파스티바_봇 사용 설명서", description="제작 - Rolling_Pumpkin, 지원 - DEEPDIVE", color=0x44e456)
+		embed.add_field(name="* 프레임데이터", value="파스티바_봇은 Dustloop wiki 에서 프레임 데이터를 가져왔습니다", inline=False)
+		embed.add_field(name="* 버튼 입력", value="약공격은 L (Light)\n중공격은 M (Middle)\n강공격은 H (Heavy)\n아무 버튼이든 상관없는 경우 X (예시 : 236X)", inline=False)
+		embed.add_field(name="* 공중 사용 기술", value="공중에서 사용하는 기술은 커맨드 앞에 j 를 붙여주세요", inline=False)
+		embed.add_field(name="* 근,원거리 기본기", value="근거리는 공격키 앞에 c. (close), 원거리는 f. (far)를 붙여주세요", inline=False)
+		embed.add_field(name="* 검색예시", value=" 검색 그랑 214H (그랑 드라이브 버스트-강)\n metera f.M (메테라 원거리 중)", inline=False)
+		embed.add_field(name="* 피드백", value="틀린 부분, 추가할 부분, 개선할 부분은 채널을 통해 말씀해주세요. \n 단, 인식 문제로 앞에 특수문자를 넣는 것을 권장합니다.", inline=False)
 		await ctx.send(embed=embed)
 
 
@@ -194,28 +168,6 @@ async def _char(ctx):
 		embed = discord.Embed(title="캐릭터 리스트", description="현재 검색 가능한 캐릭터(한글 가나다순) 목록입니다.", color=0x6b9fff)
 		for row in db.char():
 			embed.add_field(name=row['name_ko'], value=row['charname'], inline=False)
-	# embed.add_field(name=row['name_ko'], value=row['charname'], inline=False)
-	# embed.add_field(name="그랑", value="Gran", inline=True)
-	# embed.add_field(name="나루메아", value="Narmaya", inline=True)
-	# embed.add_field(name="랜슬롯", value="Lancelot", inline=True)
-	# embed.add_field(name="로아인", value="Lowain", inline=True)
-	# embed.add_field(name="메테라", value="Metera", inline=True)
-	# embed.add_field(name="바자라가", value="Vaseraga", inline=True)
-	# embed.add_field(name="벨리알", value="Belial", inline=True)
-	# embed.add_field(name="벨제붑", value="Beelzebub", inline=True)
-	# embed.add_field(name="샤를로테", value="Charlotta", inline=True)
-	# embed.add_field(name="소리즈", value="Soriz", inline=True)
-	# embed.add_field(name="우노", value="Anre", inline=True)
-	# embed.add_field(name="유스테스", value="Eustace", inline=True)
-	# embed.add_field(name="유엘", value="Yuel", inline=True)
-	# embed.add_field(name="제타", value="Zeta", inline=True)
-	# embed.add_field(name="조이", value="Zooey", inline=True)
-	# embed.add_field(name="지타", value="Djeeta", inline=True)
-	# embed.add_field(name="카타리나", value="Katalina", inline=True)
-	# embed.add_field(name="칼리오스트로", value="Cagliostro", inline=True)
-	# embed.add_field(name="파스티바", value="Ladiva", inline=True)
-	# embed.add_field(name="퍼시벌", value="Percival", inline=True)
-	# embed.add_field(name="페리", value="Ferry", inline=True)
 		await ctx.send(embed=embed)
 
 
@@ -321,11 +273,6 @@ async def _walkthrough(ctx, charname):
 		charname = db.name_ko(charname)
 		logging.debug(charname)
 		await blow.g_embed(ctx, charname)
-	# if charname in charlist:
-	# 		await blow.g_embed(ctx, charname)
-	# else:
-	# 		embed = discord.Embed(title="해당하는 정보를 찾을 수 없습니다", description="다시 한 번 확인해 주세요", color=0xedf11e)
-	# 		await ctx.send(embed=embed)
 
 
 @slash.slash(name="랜덤", description='랜덤으로 아무 캐릭터나 뽑아줍니다.', guild_ids=guild_ids)
@@ -434,6 +381,5 @@ async def validate(ctx):
 	return False
 
 
-# load_dotenv()
 bot.run(os.environ['token'])
 
